95acda12c7b36aa2ffd21a4947fd155eb43aec6d
Matilda Fahlén <matilda_fahlen@hotmail.com>
2022-10-26 18:44:52 +0200

    added questions, all done

--- main.js
+++ main.js
@@ -1,47 +1,57 @@
 let score = 0;
 
 function calc(x) {
-    if (x.value === "right") {
+    if (!x) {
+        alert("Du måste svara på alla frågor");
+    }
+    else if (x.value === "right") {
         return score++;
     }
-    else if (x.value === "wrong") {
-        
-    }
-    else {
-        alert("Du måste fylla i ett av svarsalernativen"); //Fixa denna... Funkar ej! "Kan inte hitta null"
-    }
 }
+
 let submitBtn = document.querySelector("#submit");
 let finalScore = document.querySelector("#finalScore");
 let showResult = document.createElement("h2");
 
 submitBtn.addEventListener("click", () => {
-    finalScore.innerText = ""; //Fungerar ej vet inte varför...
+    score = 0;
     calc(document.querySelector("[name='question1']:checked"));
     calc(document.querySelector("[name='question2']:checked"));
     calc(document.querySelector("[name='question3']:checked"));
-    /*    calc(document.querySelector("[name='question4']:checked").value);
-    calc(document.querySelector("[name='question5']:checked").value);
-    calc(document.querySelector("[name='question6']:checked").value);
-    calc(document.querySelector("[name='question7']:checked").value);
-    calc(document.querySelector("[name='question8']:checked").value);
-    calc(document.querySelector("[name='question9']:checked").value);
-    calc(document.querySelector("[name='question10']:checked").value); */
+    calc(document.querySelector("[name='question4']:checked"));
+    calc(document.querySelector("[name='question5']:checked"));
+    calc(document.querySelector("[name='question6']:checked"));
+    calc(document.querySelector("[name='question7']:checked"));
+    calc(document.querySelector("[name='question8']:checked"));
+    calc(document.querySelector("[name='question9']:checked"));
+    calc(document.querySelector("[name='question10']:checked"));
     
     if (score > 7) {
-        showResult.innerText = `Du fick ${score} rätt! Mycket väl godkänt. Bra jobbat!`;
+        showResult.innerText = `Du fick ${score} rätt. Mycket väl godkänt - bra jobbat!`;
         showResult.style.color = "green";
         finalScore.append(showResult);
     }
     else if (score >= 5 && score <= 7) {
-        showResult.innerText = `Du fick ${score} rätt! Godkänt`;
+        showResult.innerText = `Du fick ${score} rätt. Helt klart godkänt!`;
         showResult.style.color = "orange";
         finalScore.append(showResult);
     }
     else if (score < 5) {
-        showResult.innerText = `Du fick ${score} rätt! Underkänt. Försök igen`;
+        showResult.innerText = `Du fick ${score} rätt. Tror du kan bättre!`;
         showResult.style.color = "red";
         finalScore.append(showResult);
+        
+        let tryAgainBtn = document.createElement("button");
+        tryAgainBtn.innerText = "Försök igen";
+        finalScore.append(tryAgainBtn);
+
+        tryAgainBtn.addEventListener("click", () => {
+            let radios = document.querySelectorAll("input[type=radio]");
+            for (let i=0;i<radios.length;i++) {
+                radios[i].checked = false;
+            }
+        finalScore.innerText = "";
+        })
     } 
 })
 
@@ -64,14 +74,3 @@ function lightMode() {
     questions.style.color = "black";
     getResult.style.color = "black";
 }
-// Eventuellt onödig kod
-/*   let answer1 = document.querySelector("[name='question1']:checked").value;
-let answer2 = document.querySelector("[name='question2']:checked").value;
-let answer3 = document.querySelector("[name='question3']:checked").value;
-let answer4 = document.querySelector("[name='question4']:checked").value;
-let answer5 = document.querySelector("[name='question5']:checked").value;
-let answer6 = document.querySelector("[name='question6']:checked").value;
-let answer7 = document.querySelector("[name='question7']:checked").value;
-let answer8 = document.querySelector("[name='question8']:checked").value;
-let answer9 = document.querySelector("[name='question9']:checked").value;
-let answer10 = document.querySelector("[name='question10']:checked").value; */
