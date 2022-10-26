1130ef16b49b48d4930b15bb72aee26c05bcbfe5
Matilda Fahlén <matilda_fahlen@hotmail.com>
2022-10-26 11:42:25 +0200

    works ok

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
-let submitBtn = document.querySelector("#submit");
 let score = 0;
-let finalScore = document.querySelector("#finalScore");
-let showResult = document.createElement("h2");
 
 function calc(x) {
-    if (x === "right") {
+    if (x.value === "right") {
         return score++;
     }
-    else if (x === "wrong") {
+    else if (x.value === "wrong") {
         
     }
     else {
-        alert("Du måste fylla i ett av svarsalernativen"); //Fixa denna... Funkar ej! Träffar bara null-värde och går ej att köra-
+        alert("Du måste fylla i ett av svarsalernativen"); //Fixa denna... Funkar ej! "Kan inte hitta null"
     }
 }
+let submitBtn = document.querySelector("#submit");
+let finalScore = document.querySelector("#finalScore");
+let showResult = document.createElement("h2");
 
 submitBtn.addEventListener("click", () => {
-    calc(document.querySelector("[name='question1']:checked").value);
-    calc(document.querySelector("[name='question2']:checked").value);
-    calc(document.querySelector("[name='question3']:checked").value);
-    calc(document.querySelector("[name='question4']:checked").value);
+    finalScore.innerText = ""; //Fungerar ej vet inte varför...
+    calc(document.querySelector("[name='question1']:checked"));
+    calc(document.querySelector("[name='question2']:checked"));
+    calc(document.querySelector("[name='question3']:checked"));
+    /*    calc(document.querySelector("[name='question4']:checked").value);
     calc(document.querySelector("[name='question5']:checked").value);
     calc(document.querySelector("[name='question6']:checked").value);
     calc(document.querySelector("[name='question7']:checked").value);
     calc(document.querySelector("[name='question8']:checked").value);
     calc(document.querySelector("[name='question9']:checked").value);
-    calc(document.querySelector("[name='question10']:checked").value);
-   
+    calc(document.querySelector("[name='question10']:checked").value); */
+    
     if (score > 7) {
         showResult.innerText = `Du fick ${score} rätt! Mycket väl godkänt. Bra jobbat!`;
         showResult.style.color = "green";
@@ -44,7 +45,25 @@ submitBtn.addEventListener("click", () => {
     } 
 })
 
+function darkMode() {
+    document.body.style.background = "black";
+    let intro = document.querySelector("#intro");
+    let questions = document.querySelector("#questions");
+    let getResult = document.querySelector("#getResult");
+    intro.style.color = "white";
+    questions.style.color = "white";
+    getResult.style.color = "white";
+    }
 
+function lightMode() {
+    document.body.style.background = "white";
+    let intro = document.querySelector("#intro");
+    let questions = document.querySelector("#questions");
+    let getResult = document.querySelector("#getResult");
+    intro.style.color = "black";
+    questions.style.color = "black";
+    getResult.style.color = "black";
+}
 // Eventuellt onödig kod
 /*   let answer1 = document.querySelector("[name='question1']:checked").value;
 let answer2 = document.querySelector("[name='question2']:checked").value;
